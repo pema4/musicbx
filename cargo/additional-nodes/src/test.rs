c0fe20bef1f5a56ec87e35b64b5b8f2269869c4a
pema4 <petelin.maksim@live.com>
2022-10-15 00:53:52 +0300

    Revert auto-format in 'node' macros examples

--- cargo/additional-nodes/src/test.rs
+++ cargo/additional-nodes/src/test.rs
@@ -5,10 +5,10 @@ use musicbx::std::osc::SinOsc;
 use musicbx::FromSampleRate;
 
 #[node {
-freq -> modulator.freq,
-freq -> carrier.freq,
-modulator.output -> carrier.phase_mod,
-carrier.output -> output,
+    freq -> modulator.freq,
+    freq -> carrier.freq,
+    modulator.output -> carrier.phase_mod,
+    carrier.output -> output,
 }]
 #[derive(FromSampleRate)]
 pub struct MyNode {
